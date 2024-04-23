adb3f7936b519433031ccbe77e30ec0b56cf3fe3
saulo <sauloassis4123@gmail.com>
2024-04-22 23:35:59 -0300

    feat: building lambda layers stack

---
+++ lib/productsAppLayers-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as ssm from "aws-cdk-lib/aws-ssm"; // store params (aws system manager)
+
+export class ProductsAppLayersStack extends cdk.Stack {
+  readonly productsLayers: lambda.LayerVersion;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.productsLayers = new lambda.LayerVersion(this, "ProductsLayer", {
+      code: lambda.Code.fromAsset("lambda/products/layers/productsLayers"),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+      layerVersionName: "ProductsLayers",
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    new ssm.StringParameter(); // store one string  param
+  }
+}
